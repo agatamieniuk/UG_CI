9c53d8a55dd7102dcc8c29c59e96177f1f5c0f4e
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-08-19 18:08:30 +0200

    Inicjalizacja

---
+++ src/main/java/pl/ug/ug_ci/UgCiApplication.java
@@ -0,0 +1,13 @@
+package pl.ug.ug_ci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UgCiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UgCiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/ug/ug_ci/UgCiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ug.ug_ci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UgCiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

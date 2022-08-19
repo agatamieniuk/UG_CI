241d786ca3198ab87df82ba3d2c90a121133ee43
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-08-19 19:20:26 +0200

    Dodanie Rates.class i NBPConverterDto

---
+++ src/main/java/pl/ug/ug_ci/webclient/converter/dto/NBPConverterDto.java
@@ -0,0 +1,6 @@
+package pl.ug.ug_ci.webclient.converter.dto;
+
+public class NBPConverterDto {
+    private String code;
+    private Rates rates;
+}
---
+++ src/main/java/pl/ug/ug_ci/webclient/converter/dto/Rates.java
@@ -0,0 +1,6 @@
+package pl.ug.ug_ci.webclient.converter.dto;
+
+public class Rates {
+    private String effectiveDate;
+    private Double mid;
+}

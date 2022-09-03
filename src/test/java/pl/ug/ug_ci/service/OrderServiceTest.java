ab1bdcba6b153ca93617e7e00bccce3ca03cf409
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-09-03 22:30:04 +0200

    Dodanie TODO w testSaveOrder()

--- src/test/java/pl/ug/ug_ci/service/OrderServiceTest.java
+++ src/test/java/pl/ug/ug_ci/service/OrderServiceTest.java
@@ -92,5 +92,23 @@ public class OrderServiceTest {
         assertTrue(orderService.sortByDateDesc().get(0).getOrderPostingDate().compareTo(orderService.sortByDateDesc().get(orderService.sortByDateDesc().size() - 1).getOrderPostingDate()) >= 0);
     }
 
-    
+    @Test
+    public void testSaveOrder() {
+        Order order = new Order();
+        order.setName("komputer Agaty");
+        order.setOrderPostingDate(LocalDate.of(2022, 01, 03));
+        order.setPayInDollar(345F);
+        Order savedOrder = orderService.saveOrder(order);
+        orderService.findById(orderService.findAll().size()).getPayInPLN();
+        System.out.println(savedOrder.getPayInPLN());
+        System.out.println(orderService.findById(orderService.findAll().size()).getPayInPLN());
+
+        assertAll("SaveOrder",
+                () -> assertEquals(savedOrder.getName(), orderService.findById(orderService.findAll().size()).getName()),
+                () -> assertEquals(savedOrder.getOrderPostingDate(), orderService.findById(orderService.findAll().size()).getOrderPostingDate()),
+                () -> assertEquals(savedOrder.getPayInDollar(), orderService.findById(orderService.findAll().size()).getPayInDollar())
+//              TODO: czemu zaokrągla do 4 miejsc
+//                () -> assertEquals(savedOrder.getPayInPLN().floatValue(), orderService.findById(orderService.findAll().size()).getPayInPLN())
+        );
+    }
 }

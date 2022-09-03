5a43a6f9123282336caff3b6992ccc5ea2a54812
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-09-03 20:47:38 +0200

    Dodanie testFindBy(), testSortByNameAsc(), testSortByNameDesc()

--- src/test/java/pl/ug/ug_ci/service/OrderServiceTest.java
+++ src/test/java/pl/ug/ug_ci/service/OrderServiceTest.java
@@ -1,27 +1,16 @@
 package pl.ug.ug_ci.service;
 
-import org.aspectj.weaver.ast.Or;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import pl.ug.ug_ci.model.ConverterDto;
 import pl.ug.ug_ci.model.Order;
-import pl.ug.ug_ci.repository.OrderRepository;
-import pl.ug.ug_ci.webclient.converter.ConverterClient;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 public class OrderServiceTest {
 
@@ -29,21 +18,52 @@ public class OrderServiceTest {
     private OrderService orderService;
 
     @Test
-    public void testSaveOrder(){
+    public void testFindAll() {
 
-        Order order = new Order();
-        order.setName("komputer 1");
-        order.setPayInPLN(345F);
-        order.setOrderPostingDate(LocalDate.of(2022, 01, 03));
-        order.setPayInPLN(1394.63F);
+        Order order = new Order(1, "komputer 1", LocalDate.of(2022, 01, 03), 345F, 1394.63F);
 
-        assertAll("save order",
-                ()->assertEquals(order.getName(), orderService.findAll().get(0).getName()),
-                ()->assertEquals(order.getOrderPostingDate(), orderService.findAll().get(0).getOrderPostingDate()),
-//TODO: czemu sie wykrzacza?
-//                ()->assertEquals(order.getPayInDollar(), orderService.findAll().get(0).getPayInDollar()),
-                ()->assertEquals(order.getPayInPLN(), orderService.findAll().get(0).getPayInPLN())
+        assertAll("Find All",
+                () -> assertEquals(order.getName(), orderService.findAll().get(0).getName()),
+                () -> assertEquals(order.getOrderPostingDate(), orderService.findAll().get(0).getOrderPostingDate()),
+                () -> assertEquals(order.getPayInDollar(), orderService.findAll().get(0).getPayInDollar()),
+                () -> assertEquals(order.getPayInPLN(), orderService.findAll().get(0).getPayInPLN())
         );
+    }
 
+    @Test
+    public void testFindBy() {
+        Order order = new Order(1, "komputer 1", LocalDate.of(2022, 01, 03), 345F, 1394.63F);
+        Order order2 = new Order(2, "C komputer 1", LocalDate.of(2022, 01, 10), 345F, 1382.21F);
+
+        List<Order> orderList = new ArrayList<>();
+        orderList.add(order);
+        orderList.add(order2);
+
+        assertAll("Find By",
+                () -> assertEquals(orderList.size(), orderService.findBy("komputer 1").size()),
+                () -> assertEquals(orderList.get(0).getName(), orderService.findBy("komputer 1").get(0).getName()),
+                () -> assertEquals(orderList.get(1).getName(), orderService.findBy("komputer 1").get(1).getName()),
+                () -> assertEquals(orderList.get(0).getOrderPostingDate(), orderService.findBy("komputer 1").get(0).getOrderPostingDate()),
+                () -> assertEquals(orderList.get(1).getOrderPostingDate(), orderService.findBy("komputer 1").get(1).getOrderPostingDate()),
+                () -> assertEquals(orderList.get(0).getPayInDollar(), orderService.findBy("komputer 1").get(0).getPayInDollar()),
+                () -> assertEquals(orderList.get(1).getPayInDollar(), orderService.findBy("komputer 1").get(1).getPayInDollar()),
+                () -> assertEquals(orderList.get(0).getPayInPLN(), orderService.findBy("komputer 1").get(0).getPayInPLN()),
+                () -> assertEquals(orderList.get(1).getPayInPLN(), orderService.findBy("komputer 1").get(1).getPayInPLN())
+        );
+    }
+
+    //Poniżej korzystam z informacji, iż Comparatory zwracaja liczbę (ujemna/dodatnia/zero) ujemna – obiekt mniejszy, dodatnia – obiekt wiekszy, zero – obiekt rowny:
+    @Test
+    public void testSortByNameAsc() {
+        assertTrue(orderService.sortByNameAsc().get(0).getName().compareTo(orderService.sortByNameAsc().get(1).getName()) < 0);
+        assertTrue(orderService.sortByNameAsc().get(1).getName().compareTo(orderService.sortByNameAsc().get(2).getName()) < 0);
+        assertTrue(orderService.sortByNameAsc().get(0).getName().compareTo(orderService.sortByNameAsc().get(2).getName()) < 0);
+    }
+
+    @Test
+    public void testSortByNameDesc() {
+        assertTrue(orderService.sortByNameDesc().get(0).getName().compareTo(orderService.sortByNameDesc().get(1).getName()) > 0);
+        assertTrue(orderService.sortByNameDesc().get(1).getName().compareTo(orderService.sortByNameDesc().get(2).getName()) > 0);
+        assertTrue(orderService.sortByNameDesc().get(0).getName().compareTo(orderService.sortByNameDesc().get(2).getName()) > 0);
     }
 }
